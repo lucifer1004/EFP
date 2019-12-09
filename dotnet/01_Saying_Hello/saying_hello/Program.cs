d9bcca21f24e8a0154fbe83ac679734be8a4602b
Gabriel Wu <wuzihua@pku.edu.cn>
2019-12-09 17:22:30 +0800

    dotnet: 01

---
+++ dotnet/01_Saying_Hello/saying_hello/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myApp
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("What is your name? ");
+      var name = Console.ReadLine();
+      Console.WriteLine("Hello, " + name + ", nice to meet you!");
+    }
+  }
+}  // namespace myApp
